bab33490777765e51656f93a9b47155860db81d9
sudesh <sudesh@foreignerds.com>
2021-01-21 09:44:56 +0530

    theam file added

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,268 @@
 <template>
   <div>
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="~/assets/img/post-1.jpg" alt="First slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="~/assets/img/post-2.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="~/assets/img/post-3.jpg" alt="Third slide">
-        </div>
+   
+    <div class="">
+      <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+                <div class="post post-thumb">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-1.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-2" href="category.html">JavaScript</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="post post-thumb">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-2.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-3" href="category.html">Jquery</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                  <h2>Recent Posts</h2>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-3.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-1" href="category.html">Web Design</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-4.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-2" href="category.html">JavaScript</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-5.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-3" href="category.html">Jquery</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="clearfix visible-md visible-lg"></div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-6.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-2" href="category.html">JavaScript</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-1.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-4" href="category.html">Css</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="post">
+                  <a class="post-img" href="blog-post.html"><img src="~assets/img/post-2.jpg" alt=""></a>
+                  <div class="post-body">
+                      <div class="post-meta">
+                        <a class="post-category cat-1" href="category.html">Web Design</a>
+                        <span class="post-date">March 27, 2018</span>
+                      </div>
+                      <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                  <div class="col-md-12">
+                      <div class="post post-thumb">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-2.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-3" href="category.html">Jquery</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-1.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-4" href="category.html">Css</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">CSS Float: A Tutorial</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-2.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-1" href="category.html">Web Design</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="clearfix visible-md visible-lg"></div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-4.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-2" href="category.html">JavaScript</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-5.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-3" href="category.html">Jquery</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="clearfix visible-md visible-lg"></div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-3.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-1" href="category.html">Web Design</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="post">
+                        <a class="post-img" href="blog-post.html"><img src="~assets/img/post-4.jpg" alt=""></a>
+                        <div class="post-body">
+                            <div class="post-meta">
+                              <a class="post-category cat-2" href="category.html">JavaScript</a>
+                              <span class="post-date">March 27, 2018</span>
+                            </div>
+                            <h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+                        </div>
+                      </div>
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="aside-widget">
+                  <div class="section-title">
+                      <h2>Most Read</h2>
+                  </div>
+                  <div class="post post-widget">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/widget-1.jpg" alt=""></a>
+                      <div class="post-body">
+                        <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+                      </div>
+                  </div>
+                  <div class="post post-widget">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/widget-2.jpg" alt=""></a>
+                      <div class="post-body">
+                        <h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+                      </div>
+                  </div>
+                  <div class="post post-widget">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/widget-3.jpg" alt=""></a>
+                      <div class="post-body">
+                        <h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
+                      </div>
+                  </div>
+                  <div class="post post-widget">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/widget-4.jpg" alt=""></a>
+                      <div class="post-body">
+                        <h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+                      </div>
+                  </div>
+                </div>
+                <div class="aside-widget">
+                  <div class="section-title">
+                      <h2>Featured Posts</h2>
+                  </div>
+                  <div class="post post-thumb">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/post-2.jpg" alt=""></a>
+                      <div class="post-body">
+                        <div class="post-meta">
+                            <a class="post-category cat-3" href="category.html">Jquery</a>
+                            <span class="post-date">March 27, 2018</span>
+                        </div>
+                        <h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+                      </div>
+                  </div>
+                  <div class="post post-thumb">
+                      <a class="post-img" href="blog-post.html"><img src="~assets/img/post-1.jpg" alt=""></a>
+                      <div class="post-body">
+                        <div class="post-meta">
+                            <a class="post-category cat-2" href="category.html">JavaScript</a>
+                            <span class="post-date">March 27, 2018</span>
+                        </div>
+                        <h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
+                      </div>
+                  </div>
+                </div>
+                <div class="aside-widget text-center">
+                  <a href="#" style="display: inline-block;margin: auto;">
+                  <img class="img-responsive" src="~assets/img/ad-1.jpg" alt="">
+                  </a>
+                </div>
+            </div>
+          </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-    <div class="container">
-      
     </div>
   </div>  
 </template>
@@ -32,42 +272,5 @@ export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
